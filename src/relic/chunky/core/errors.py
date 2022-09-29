b73a2d076e7cca7156ab929127598c504e4e6628
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-09-29 12:18:23 -0800

    Refactor to use new FS api

--- src/relic/chunky/core/errors.py
+++ src/relic/chunky/core/errors.py
@@ -1,39 +1,64 @@
-from typing import Union
+from typing import Union, List, Optional
 
-from relic.chunky.core._core import ChunkType, Version
-from relic.core.errors import MismatchError
+from relic.chunky.core.definitions import ChunkType, Version
+from relic.core.errors import MismatchError, RelicToolError
 
 
-class ChunkError(Exception):
+class ChunkError(RelicToolError):
     pass
 
 
 class ChunkTypeError(ChunkError):
-    def __init__(self, chunk_type: Union[bytes, str] = None, *args):
+    def __init__(self, chunk_type: Optional[Union[bytes, str]] = None, *args:object) -> None:
         super().__init__(*args)
         self.chunk_type = chunk_type
 
-    def __str__(self):
+    def __str__(self) -> str:
         msg = f"ChunkType must be {repr(ChunkType.Folder.value)} or {repr(ChunkType.Data.value)}"
         if not self.chunk_type:
             return msg + "!"
-        else:
-            return msg + f"; got {repr(self.chunk_type)}!"
+        return msg + f"; got {repr(self.chunk_type)}!"
 
 
 class ChunkNameError(ChunkError):
-    def __init__(self, name: Union[bytes, str] = None, *args):
+    def __init__(self, name: Optional[Union[bytes, str]] = None, *args:object) -> None:
         super().__init__(*args)
         self.name = name
 
-    def __str__(self):
-        msg = f"Chunk name was not parsable ascii text"
+    def __str__(self) -> str:
+        msg = "Chunk name was not parsable ascii text"
         if not self.name:
             return msg + "!"
-        else:
-            return msg + f"; got {repr(self.name)}!"
+        return msg + f"; got {repr(self.name)}!"
 
 
-class VersionMismatchError(MismatchError):
-    def __init__(self, received: Version = None, expected: Version = None):
+class VersionMismatchError(MismatchError[Version]):
+    def __init__(self, received: Optional[Version] = None, expected: Optional[Version] = None):
         super().__init__("Version", received, expected)
+
+
+class VersionNotSupportedError(RelicToolError):
+    """
+    An unknown version was provided.
+    """
+
+    def __init__(self, received: Version, allowed: List[Version]):
+        super().__init__()
+        self.received = received
+        self.allowed = allowed
+
+    def __str__(self) -> str:
+        def str_ver(version: Version) -> str:  # dont use str(version); too verbose
+            return f"{version.major}.{version.minor}"
+
+        allowed_str = [str_ver(_) for _ in self.allowed]
+        return f"Version `{str_ver(self.received)}` is not supported. Versions supported: `{allowed_str}`"
+
+
+__all__ = [
+    "ChunkError",
+    "ChunkTypeError",
+    "ChunkNameError",
+    "VersionMismatchError",
+    "VersionNotSupportedError"
+]
